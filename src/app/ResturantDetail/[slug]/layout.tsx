295e3226bbedfed268124a6d9b6a6f258d963d7c
Rafi Shahriar <cm.rafi.shahriar@gmail.com>
2023-11-24 01:08:38 +0600

    new

--- src/app/ResturantDetail/[slug]/layout.tsx
+++ src/app/ResturantDetail/[slug]/layout.tsx
@@ -9,15 +9,12 @@ export default function RestaurantLayout({
     params:{slug:string}
 
   }) {
-    console.log("restaurant_",params);
     return(
         <main className="bg-gray-100 min-h-screen w-screen">
         <main className="max-w-screen-2xl m-auto bg-white">
-          {/* NAVBAR */}
-          {/* <Navbar/> */}
-          {/* NAVBAR */} {/* HEADER */}
+
           <Header title={params.slug}/>
-          {/* HEADER */} {/* DESCRIPTION PORTION */}
+
             {children}
         </main>
         </main>
